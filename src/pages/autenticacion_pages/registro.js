9fb9204bb5531a08cc1d3a4a9f19312c9efcf93f
Jose Luis Matos Sosa <142519959+Jose-Dev02@users.noreply.github.com>
2024-12-23 08:32:02 -0500

    Update registro.js

--- src/pages/autenticacion_pages/registro.js
+++ src/pages/autenticacion_pages/registro.js
@@ -150,7 +150,7 @@ const Registro = () => {
                 {" "}
                 <IconButton onClick={handleVisibility}>
                   {" "}
-                  {visibility ? <VisibilityIcon /> : <VisibilityOffIcon />}{" "}
+                  {!visibility ? <VisibilityIcon /> : <VisibilityOffIcon />}{" "}
                 </IconButton>{" "}
               </InputAdornment>
             ),
